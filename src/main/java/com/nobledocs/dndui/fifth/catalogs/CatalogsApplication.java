95d5e1f99fef68b86f1fd2b60955b6dd851ea026
AlainCarmona <alaincarmonahdz@gmail.com>
2017-05-07 20:46:47 -0500

    2017-05-07: Se modifica el POM para configurar la ejecución en el PaaS

--- src/main/java/com/nobledocs/dndui/fifth/catalogs/CatalogsApplication.java
+++ src/main/java/com/nobledocs/dndui/fifth/catalogs/CatalogsApplication.java
@@ -2,12 +2,19 @@ package com.nobledocs.dndui.fifth.catalogs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CatalogsApplication {
+public class CatalogsApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CatalogsApplication.class, args);
+    }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CatalogsApplication.class);
     }
     
 }
